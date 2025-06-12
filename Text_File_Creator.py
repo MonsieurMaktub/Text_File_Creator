79b5a45002121c5f7af2aab3e4da237e81540290
MonsiuerMaktub <mack.murphy2492@gmail.com>
2025-06-12 18:58:41 -0400

    Updated text inputs

--- Text_File_Creator.py
+++ Text_File_Creator.py
@@ -3,4 +3,4 @@ with open('example.txt', 'w') as file:
     file.write(f"{name} created this file")
 color = input("What's your favorite color? \n")
 with open('example.txt', 'a') as file:
-    file.write(f"\nFavorite color: {color}")
+    file.write(f"\n{color} is my favorite color")
