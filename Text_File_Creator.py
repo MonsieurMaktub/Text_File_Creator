70c20002199c35bc0b847dec87319cc782e885c8
MonsiuerMaktub <mack.murphy2492@gmail.com>
2025-06-12 19:11:48 -0400

    updated text

---
+++ Text_File_Creator.py
@@ -0,0 +1,6 @@
+name = input("What's your name? \n")
+with open('example.txt', 'w') as file:
+    file.write(f"{name} created this file")
+color = input("What's your favorite color? \n")
+with open('example.txt', 'a') as file:
+    file.write(f"\n{color} is {name}'s favorite color")
